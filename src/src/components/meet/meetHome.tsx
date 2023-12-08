c9db787b0b85bd728cf86787840e8bb9d4f504d3
Antônio Apolinário Gonzaga Neto <120532863+Apolinar1o@users.noreply.github.com>
2023-12-07 22:44:54 -0300

    aula 3 parte 3

---
+++ src/src/components/meet/meetHome.tsx
@@ -0,0 +1,10 @@
+
+export const Meethome = () => {
+    return (
+        <div className="container-principal">
+            <div className="container-meet">
+                <h1>asd</h1>
+            </div>
+        </div>
+    )
+}
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "../src/components/general/Footer"
 import { Header } from "../src/components/general/header"
+import { Meethome } from "../src/components/meet/meetHome"
 
 
 export const Home = () => {
@@ -7,6 +8,7 @@ export const Home = () => {
     return (
         <>  
                 <Header/>
+                <Meethome/>
                 <Footer/>
         </>
         )
